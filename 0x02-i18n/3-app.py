063cb73405d5df1fc712a7ed72e514e39d418053
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-05-14 20:04:54 +0000

    fixing .3 comments

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -7,7 +7,8 @@ from flask_babel import Babel
 
 
 class Config:
-    '''class configuration for babel'''
+    '''class configuration for babel
+    '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -20,13 +21,15 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    '''get the best match language'''
+    '''get the best match language
+    '''
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
 def hello_world() -> str:
-    '''rendering hello world page'''
+    '''rendering hello world page
+    '''
     return render_template('3-index.html')
 
 
