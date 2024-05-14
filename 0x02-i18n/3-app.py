0024868ab2ccf2c1ebc68b6b523aa1ca0afa2260
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-05-14 20:03:13 +0000

    fixing .3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -13,19 +13,19 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
 def get_locale() -> str:
     '''get the best match language'''
     return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app)
-babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route('/')
-def hello_world():
+def hello_world() -> str:
     '''rendering hello world page'''
     return render_template('3-index.html')
 
