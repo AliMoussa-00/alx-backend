e072dfef83b5e547c100bcbdefb0e2cea3517221
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-06-10 19:43:50 +0000

    adding tasks .0 - .10

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,28 @@
+import { createClient, print } from 'redis';
+import {promisify} from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+	console.log('Redis client connected to the server');
+});
+client.on('error', err => {
+	console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+const setNewSchool = (schoolName, value) => {
+	client.set(schoolName, value, print);
+};
+
+const getAsync = promisify(client.get).bind(client);
+
+const displaySchoolValue = async (schoolName) => {
+	const value = await getAsync(schoolName);
+	console.log(value);
+};
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,19 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const noti = {
+  phoneNumber: '123456789',
+  message: 'Notification Message',
+};
+
+const job = queue.create('push_notification_code', noti)
+	.save((err) => {
+		if (!err) console.log(`Notification job created: ${job.id}`)
+	})
+
+job.on('complete', () => {
+	console.log('Notification job completed')
+
+}).on('failed', () => {
+	console.log('Notification job failed')
+})
